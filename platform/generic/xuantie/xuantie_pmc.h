96f662b167f98affa544beec285f60b584a6e7ef
Chen Pei <cp0613@linux.alibaba.com>
2025-01-16 16:03:27 +0800

    platform: Add xuantie_pmc for PMC test
    This is an example implementation of power management using C907 PMC, which mainly implements sbi_hsm_device, sbi_system_reset_device and sbi_system_suspend_device defined by opensbi.
    
    DTS example:
    ```
    {
    	model = "xuantie,dummy";
    	compatible = "xuantie,dummy";
    
    	pmc: xuantie_ipmc@26900000 {
    		compatible = "xuantie,pmc";
    		status = "okay";
    		reg = <0x00000000 0x26900000 0x00000000 0x00001000>;
    	};
    }
    ```
    
    Signed-off-by: Chen Pei <cp0613@linux.alibaba.com>

---
+++ platform/generic/xuantie/xuantie_pmc.h
@@ -0,0 +1,52 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __XUANTIE_PMC_H__
+#define __XUANTIE_PMC_H__
+
+#include <sbi/sbi_types.h>
+
+/* clang-format off */
+
+#define XUANTIE_DCACHE_INVAL_CLEAN_ALL	".long 0x0030000b"
+#define XUANTIE_SYNC_S					".long 0x0190000b"
+
+/*
+ core    PMC: blk1 = VFPU; blk2 = MPU
+ cluster PMC: blk1 = l2cache
+ */
+#define PMC_BLK_CORE		0
+#define PMC_BLK_CORE_VFPU	1
+#define PMC_BLK_CORE_MPU	2
+#define PMC_BLK_CLUSTER		0
+#define PMC_BLK_CLUSTER_L2	1
+
+#define PMC_TRANS_MODE_SHIFT		4
+#define PMC_TRANS_MODE_DIRECT		(_UL(1) << PMC_TRANS_MODE_SHIFT)
+#define PMC_TRANS_MODE_DYNAMIC		(_UL(0) << PMC_TRANS_MODE_SHIFT)
+/*pmc_blk_mode_config (basic type)*/
+#define PMC_BLK_MODE_CONFIG_0		0 // OFF pstate enable
+#define PMC_BLK_MODE_CONFIG_1		1
+#define PMC_BLK_MODE_CONFIG_2		2
+#define PMC_BLK_MODE_CONFIG_3		3
+#define PMC_BLK_MODE_CONFIG_4		4
+#define PMC_BLK_MODE_CONFIG_5		5
+#define PMC_BLK_MODE_CONFIG_6		6
+#define PMC_BLK_MODE_CONFIG_7		7
+#define PMC_BLK_MODE_CONFIG_8		8 // ON pstate enable
+#define PMC_BLK_MODE_CONFIG_9		9
+#define PMC_BLK_MODE_CONFIG_ON		PMC_BLK_MODE_CONFIG_8
+#define PMC_BLK_MODE_CONFIG_OFF		PMC_BLK_MODE_CONFIG_0
+/*pmc_blk_mode_config (pe type)*/
+#define PMC_BLK_LOW_POWER_STATE_OFF 0
+#define PMC_BLK_LOW_POWER_STATE_RET 1
+
+
+/* clang-format on */
+
+struct xuantie_pmc_data {
+	unsigned long pmc_addr;
+};
+
+#endif /* __XUANTIE_PMC_H__ */
