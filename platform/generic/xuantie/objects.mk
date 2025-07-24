ce6ec8f498b8f8f4e456d5ce8877360d7b0202a6
Chen Pei <cp0613@linux.alibaba.com>
2025-07-24 20:06:03 +0800

    platform: xuantie: Add xuantie link pmu support
    Signed-off-by: Chen Pei <cp0613@linux.alibaba.com>

--- platform/generic/xuantie/objects.mk
+++ platform/generic/xuantie/objects.mk
@@ -3,4 +3,4 @@
 #
 
 carray-platform_override_modules-$(CONFIG_PLATFORM_XUANTIE) += xuantie_dummy
-platform-objs-$(CONFIG_PLATFORM_XUANTIE) += xuantie/xuantie_dummy.o xuantie/xuantie_pmc.o
+platform-objs-$(CONFIG_PLATFORM_XUANTIE) += xuantie/xuantie_dummy.o xuantie/xuantie_pmc.o xuantie/xuantie_link.o
