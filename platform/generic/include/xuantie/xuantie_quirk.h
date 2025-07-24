6234d69535c1734bfb2851a3a8941e68fe6d2001
Chen Pei <cp0613@linux.alibaba.com>
2025-07-24 20:06:03 +0800

    platform: xuantie: Add xuantie link pmu support
    Signed-off-by: Chen Pei <cp0613@linux.alibaba.com>

--- platform/generic/include/xuantie/xuantie_quirk.h
+++ platform/generic/include/xuantie/xuantie_quirk.h
@@ -5,7 +5,8 @@
 #ifndef __RISCV_XUANTIE_QUIRK_H__
 #define __RISCV_XUANTIE_QUIRK_H__
 
-#define QUIRK_XUANTIE_PMC BIT(0)
+#define QUIRK_XUANTIE_PMC  BIT(0)
+#define QUIRK_XUANTIE_LINK BIT(1)
 
 struct xuantie_generic_quirks {
 	u32 quirk;
--- platform/generic/xuantie/xuantie_dummy.c
+++ platform/generic/xuantie/xuantie_dummy.c
@@ -12,11 +12,17 @@
 
 #include <xuantie/xuantie_quirk.h>
 #include <xuantie/xuantie_pmc.h>
+#include <xuantie/xuantie_link.h>
+
+static u32 gquirk = 0;
 
 int xuantie_final_init(bool cold_boot)
 {
 	if (cold_boot) {
-		xuantie_pmc_device_init();
+		if (gquirk & QUIRK_XUANTIE_PMC)
+			xuantie_pmc_device_init();
+		if (gquirk & QUIRK_XUANTIE_LINK)
+			xuantie_link_pmu_device_init();
 	}
 
 	return generic_final_init(cold_boot);
@@ -27,19 +33,28 @@ static int xuantie_dummy_platform_init(const void *fdt, int nodeoff,
 {
 	const struct xuantie_generic_quirks *data = match->data;
 
-	if (data->quirk & QUIRK_XUANTIE_PMC)
-		generic_platform_ops.final_init = xuantie_final_init;
+	gquirk = data->quirk;
+	generic_platform_ops.final_init = xuantie_final_init;
 
 	return 0;
 }
+
+static const struct xuantie_generic_quirks xuantie_quirks = {
+	.quirk = QUIRK_XUANTIE_PMC | QUIRK_XUANTIE_LINK,
+};
 
 static const struct xuantie_generic_quirks xuantie_pmc_quirks = {
 	.quirk = QUIRK_XUANTIE_PMC,
 };
 
+static const struct xuantie_generic_quirks xuantie_link_quirks = {
+	.quirk = QUIRK_XUANTIE_LINK,
+};
+
 static const struct fdt_match xuantie_dummy_match[] = {
-	{ .compatible = "xuantie,dummy", .data = &xuantie_pmc_quirks },
+	{ .compatible = "xuantie,dummy", .data = &xuantie_quirks },
 	{ .compatible = "xuantie,pmc", .data = &xuantie_pmc_quirks },
+	{ .compatible = "xuantie,link", .data = &xuantie_link_quirks },
 	{ },
 };
 
