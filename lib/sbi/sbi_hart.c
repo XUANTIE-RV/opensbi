3abb8f805445b7fba2470c2b67a326d1f3b9702d
Chen Pei <cp0613@linux.alibaba.com>
2025-09-23 13:59:58 +0800

    [TODO] smmtt: Config msdcfg
    SSM=0; SRL=SML=5
    
    TODO: check smmtt available
    
    Signed-off-by: Chen Pei <cp0613@linux.alibaba.com>
    Change-Id: Ifbda146b7d95917969210ed09b3109e23a5d59f7

--- lib/sbi/sbi_hart.c
+++ lib/sbi/sbi_hart.c
@@ -756,6 +756,7 @@ int sbi_hart_reinit(struct sbi_scratch *scratch)
 int sbi_hart_init(struct sbi_scratch *scratch, bool cold_boot)
 {
 	int rc;
+	struct sbi_trap_info trap = {0};
 
 	/*
 	 * Clear mip CSR before proceeding with init to avoid any spurious
@@ -786,6 +787,14 @@ int sbi_hart_init(struct sbi_scratch *scratch, bool cold_boot)
 	rc = delegate_traps(scratch);
 	if (rc)
 		return rc;
+
+	// TODO: check smmtt available
+	csr_read_allowed(CSR_MSDCFG, &trap);
+	if (!trap.cause) {
+		// SSM=0; SRL=SML=5
+		csr_clear(CSR_MSDCFG, MSDCFG_SSM);
+		csr_set(CSR_MSDCFG, (5 << MSDCFG_SRL_SHIFT) | (5 << MSDCFG_SML_SHIFT));
+	}
 
 	return sbi_hart_reinit(scratch);
 }
